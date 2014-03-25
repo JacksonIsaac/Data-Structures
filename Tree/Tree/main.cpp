c17e2b06a63e238c0c1a5c8820da6af1c05d4f60
Jackson Isaac <jacksonisaac2008@gmail.com>
2014-03-26 01:48:21 +0530

    Update main program to delete node and print levelorder

--- Tree/Tree/main.cpp
+++ Tree/Tree/main.cpp
@@ -18,11 +18,15 @@ int main(){
 				cin>>data;
 				t.insert(data);
 			}
-			//else if(in == "delete")		t.delete_node(data);
+			else if(in == "delete")
+			{
+				cin>>data;
+				t.delete_node(data);
+			}
 			else if(in == "inorder")	t.inorder();
 			else if(in == "postorder")	t.postorder();
 			else if(in == "preorder")	t.preorder();
-			//else if(in == "levelorder")	t.levelorder();
+			else if(in == "levelorder")	t.levelorder();
 			else if(in == "clear")		t.clear();
 			else if(in == "exit")		break;
 			else						cerr << "Invalid Input" << endl;
