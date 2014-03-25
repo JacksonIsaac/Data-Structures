62a2b43e0ad112eacda6087ccb5d8a9e698e9254
Jackson Isaac <jacksonisaac2008@gmail.com>
2014-03-26 02:11:56 +0530

    Declare utility functions as private methods

--- Tree/Tree/Tree_Int.h
+++ Tree/Tree/Tree_Int.h
@@ -16,6 +16,45 @@ class Tree_Int
 private:
 	Node* root;
 	size_t tree_size;
+	
+	/* Utility functions */
+	
+	/* Inorder predecessor and succesor */
+	Node* get_inorder_successor(Node* node);
+	Node* get_inorder_successor(int value);
+	
+	Node* get_inorder_predecessor(Node* node);
+	Node* get_inorder_predecessor(int value);
+	
+	/* Get parent of the node */
+	Node* get_parent(Node* node);
+	Node* get_parent(int value);
+	
+	/* Check if node is left or right child */
+	bool is_left_child(Node* node);
+	bool is_left_child(int value);
+	
+	bool is_right_child(Node* node);
+	bool is_right_child(int value);
+	
+	/* Check if the node is root node */
+	bool is_root(Node* node);
+	bool is_root(int value);
+	
+	/* Check if the node has two child nodes */
+	bool has_two_siblings(Node* node);
+	bool has_two_siblings(int value);
+	
+	/* Check if the node has only one child node */
+	bool has_only_one_sibling(Node* node);
+	bool has_only_one_sibling(int value);
+	
+	/* Check if the node is a leaf node */
+	bool is_leaf(Node* node);
+	bool is_leaf(int value);
+	
+	/* End of Utility functions */
+	
 public:
 	/* Constructor	*/
 	Tree_Int():root(NULL),tree_size(0)	{ }
@@ -66,41 +105,7 @@ public:
 
 	/* Search a node in the BST */
 	Node* search(int key);
-
-	/* Inorder predecessor and succesor */
-	Node* get_inorder_successor(Node* node);
-	Node* get_inorder_successor(int value);
-
-	Node* get_inorder_predecessor(Node* node);
-	Node* get_inorder_predecessor(int value);
-
-	/* Get parent of the node */
-	Node* get_parent(Node* node);
-	Node* get_parent(int value);
-
-	/* Check if node is left or right child */
-	bool is_left_child(Node* node);
-	bool is_left_child(int value);
-
-	bool is_right_child(Node* node);
-	bool is_right_child(int value);
-
-	/* Check if the node is root node */
-	bool is_root(Node* node);
-	bool is_root(int value);
-
-	/* Check if the node has two child nodes */
-	bool has_two_siblings(Node* node);
-	bool has_two_siblings(int value);
-
-	/* Check if the node has only one child node */
-	bool has_only_one_sibling(Node* node);
-	bool has_only_one_sibling(int value);
-
-	/* Check if the node is a leaf node */
-	bool is_leaf(Node* node);
-	bool is_leaf(int value);
-
+	
 	/* Delete node with a specific value */
 	bool delete_node(Node* node);
 	bool delete_node(int value);
